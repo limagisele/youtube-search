9da86b7de46d529cb3e913bfe6edea999149670a
limagisele <limagisele05@gmail.com>
2022-08-13 12:05:39 +1000

    created new firebase proj

--- pages/api/firebase-config.js
+++ pages/api/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "search-7c613.firebaseapp.com",
-  projectId: "search-7c613",
-  storageBucket: "search-7c613.appspot.com",
-  messagingSenderId: "4098071392",
-  appId: "1:4098071392:web:6731b46d89334856a05c05",
-  measurementId: "G-32NTS9Z454",
+  authDomain: "nextjs-fcd5d.firebaseapp.com",
+  projectId: "nextjs-fcd5d",
+  storageBucket: "nextjs-fcd5d.appspot.com",
+  messagingSenderId: "56297671587",
+  appId: "1:56297671587:web:7981eea4d89b9bd454a816",
+  measurementId: "G-4QWKMBP01Z",
 };
 
 const app = initializeApp(firebaseConfig);
